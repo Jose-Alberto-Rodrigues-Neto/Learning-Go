927bc0a50a807292bbeccc852838e924f6421289
Jose-Alberto-Rodrigues-Neto <allio.professional@gmail.com>
2024-03-23 10:26:18 -0300

    adicionando nix-shell para criar ambiente de desenvolvimento remoto em linux ou wsl

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{pkgs ? import <nixpkgs> {}
+}:
+pkgs.mkShell{
+    nam = "ambiente de desenvolvimento";
+    buildInputs = [
+        pkgs.go
+    ];
+    shellHook = ''
+        echo "Comece a desenvolver!" 
+    '';
+}
